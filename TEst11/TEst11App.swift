945d8942ed10e6eb27e52123cec455d215f23a05
fayzah <ffowz110@hotmail.com>
2024-04-29 22:14:49 +0300

    Initial Commit

---
+++ TEst11/TEst11App.swift
@@ -0,0 +1,17 @@
+//
+//  TEst11App.swift
+//  TEst11
+//
+//  Created by Faizah Almalki on 20/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TEst11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
